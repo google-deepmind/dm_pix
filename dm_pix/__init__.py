ba425708d3e6025073b9cecc3e0903bfff749c88
Claudio Fantacci <cfantacci@google.com>
2021-11-01 04:58:31 -0700

    Bump version to 0.2.0
    PiperOrigin-RevId: 406799443

--- dm_pix/__init__.py
+++ dm_pix/__init__.py
@@ -20,7 +20,7 @@ from dm_pix._src import interpolation
 from dm_pix._src import metrics
 from dm_pix._src import patch
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # Augmentation.
 adjust_brightness = augment.adjust_brightness
